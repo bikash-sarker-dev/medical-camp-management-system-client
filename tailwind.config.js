d8d5b2fa592ed399067d0caf4f856ba7f56ddec1
bikash sarker <bikash.webdeveloper5@gmail.com>
2025-01-14 10:11:22 +0600

    taiwind css colors configration and container setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,15 @@ module.exports = withMT({
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      "camp-text": "#090c15",
+      "camp-background": "#f7fafd",
+      "camp-primary": "#4c71c8",
+      "camp-secondary": "#84a0e1",
+      "camp-accent": "#5b81e1",
+      "camp-info": "#D5DFF2",
+      "camp-default": "#EAEEF1",
+    },
     container: {
       center: true,
       padding: {
