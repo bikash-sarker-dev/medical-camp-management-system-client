6bf38237b19bb8cde7977f0ef39c644b71a70062
bikash sarker <bikash.webdeveloper5@gmail.com>
2025-05-01 21:56:03 +0600

    blog change btn

--- src/components/Home/blog/BlogCard.jsx
+++ src/components/Home/blog/BlogCard.jsx
@@ -22,7 +22,11 @@ const BlogCard = ({ blog }) => {
       </CardBody>
       <CardFooter className="pt-0">
         <a href="#" className="inline-block">
-          <Button size="sm" variant="text" className="flex items-center gap-2">
+          <Button
+            size="sm"
+            variant="text"
+            className="flex items-center bg-camp-default gap-2"
+          >
             Learn More
             <svg
               xmlns="http://www.w3.org/2000/svg"
